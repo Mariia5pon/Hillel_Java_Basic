278bf51cd9a87b47ad369a66c288b580c2fc0c96
mariia <mariia@mail.uni-paderborn.de>
2025-04-27 21:30:14 +0200

    Homework 3.1 - Update printed text

--- src/com/gmail/ponomarenko/homeworks/homework03/ShopOnline.java
+++ src/com/gmail/ponomarenko/homeworks/homework03/ShopOnline.java
@@ -2,6 +2,6 @@ package com.gmail.ponomarenko.homeworks.homework03;
 
 public class ShopOnline {
     public static void main(String[] args) {
-        System.out.println("This is shop-online app");
+        System.out.println("Shop-online for customers");
     }
 }
