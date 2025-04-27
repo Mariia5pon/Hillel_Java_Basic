21503972cc84634223bc725aa4f7e33039ccbdc2
mariia <mariia@mail.uni-paderborn.de>
2025-04-27 17:01:59 +0200

    Lecture 1 - Add new class 'Lecture01'

---
+++ src/com.gmail.ponomarenko/lectures/Lecture01.java
@@ -0,0 +1,12 @@
+package lectures;
+
+public class Lecture01 {
+    public static void main(String [] args) {            // psvm + Tab = main method, точка входу (JUST ONE FOR A CLASS)
+        System.out.println("Hello World!");
+        System.out.println("My name is Mariia.");
+        System.out.print("My name: ");                   // PRINT
+        System.out.println("Mariia.");                   // PRINTLN
+    }
+}
+
+// folder "out" save all that was compiled. When you reopen your program, this folder will be renewed.
