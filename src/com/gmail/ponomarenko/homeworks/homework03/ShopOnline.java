31fec0d13e80160524b3cb4f0898c7b9498195c1
mariia <mariia@mail.uni-paderborn.de>
2025-05-05 17:22:28 +0200

    Homework 4.1 - Change package

--- src/com/gmail/ponomarenko/homeworks/homework03/ShopOnline.java
+++
@@ -1,7 +0,0 @@
-package com.gmail.ponomarenko.homeworks.homework03;
-
-public class ShopOnline {
-    public static void main(String[] args) {
-        System.out.println("Shop-online for customers");
-    }
-}
