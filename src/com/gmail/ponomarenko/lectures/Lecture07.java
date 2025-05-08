08304cf2cc4da466d1395ab73ad8a9fb5e96ae8e
mariia <mariia@mail.uni-paderborn.de>
2025-05-08 19:48:19 +0200

    Lecture 07 - for-each using

--- src/com/gmail/ponomarenko/lectures/Lecture07.java
+++ src/com/gmail/ponomarenko/lectures/Lecture07.java
@@ -14,17 +14,21 @@ public class Lecture07 {
         System.out.println("Average temperature == " + average);
          */
 
-        int[] temperatures = new int[5];
-        temperatures[0] = 3;
-        temperatures[1] = 12;
-        temperatures[2] = 10;
-        temperatures[3] = 13;
-        temperatures[4] = 15;
+//        int[] temperatures = new int[5];
+//        temperatures[0] = 3;
+//        temperatures[1] = 12;
+//        temperatures[2] = 10;
+//        temperatures[3] = 13;
+//        temperatures[4] = 15;
+
+        int[] temperatures = {3, 12, 10, 13, 15};
 
         int[] previousTemperatures = new int[100];
 
         print(previousTemperatures); // [0, 0, 0, ... 0]
         print(temperatures);
+        print(new int[]{1, 2, 3, 4});
+
         long sum = 0;
         for (int i=0; i < temperatures.length; i++){
             sum += temperatures[i];
@@ -39,6 +43,10 @@ public class Lecture07 {
         System.out.print("Temperatures: [");
         for (int i = 0; i < temperatures.length; i++) {
             System.out.print(temperatures[i] + ", ");
+        }
+        // ---= FOR EACH =---
+        for ( int temperature: temperatures) {
+            System.out.print(temperature + ", ");
         }
         System.out.print("]\n");
     }
