1d17abc82640149464bbf81e23609d7954ef77b8
mariia <mariia@mail.uni-paderborn.de>
2025-05-08 19:07:53 +0200

    Lecture 07 - Arrays creation

--- src/com/gmail/ponomarenko/lectures/Lecture07.java
+++ src/com/gmail/ponomarenko/lectures/Lecture07.java
@@ -2,6 +2,25 @@ package com.gmail.ponomarenko.lectures;
 
 public class Lecture07 {
     public static void main(String[] args) {
+        /* ---= TOO BAD =---
+        int temp = 16;
+        int yesterdayTemp = 12;
+        int dayBeforeYesterdayTemperature = 10;
 
+        int size = 3;
+
+        int average = (temp + yesterdayTemp + dayBeforeYesterdayTemperature) / size;
+
+        System.out.println("Average temperature == " + average);
+         */
+
+        int[] temperatures = new int[3];
+        temperatures[0] = 3;
+        temperatures[1] = 12;
+        temperatures[2] = 10;
+
+        int average = (temperatures[0] + temperatures[1] + temperatures[2] / temperatures.length);
+
+        System.out.println("Average temperature ==" + average);
     }
 }
