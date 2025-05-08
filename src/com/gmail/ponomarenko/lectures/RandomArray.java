37571586f8e0628183963e2c9b5044f6fa01f76a
mariia <mariia@mail.uni-paderborn.de>
2025-05-08 20:19:50 +0200

    Lecture 07 - Generate random

---
+++ src/com/gmail/ponomarenko/lectures/RandomArray.java
@@ -0,0 +1,22 @@
+package com.gmail.ponomarenko.lectures;
+
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class RandomArray {
+    public static void main(String[] args) {
+        int[] array = new int[10];
+
+        Random random = new Random(10);
+
+        for (int i = 0; i < args.length; i++) {
+            array[i] = random.nextInt(-10_000, 10_000);
+            System.out.println(array[i]);
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = ThreadLocalRandom.current().nextInt(-10000, 10000);
+            System.out.println(array[i]);
+        }
+    }
+}
