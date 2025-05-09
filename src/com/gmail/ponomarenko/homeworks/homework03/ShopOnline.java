9ed2ff21d743d0727ce0b929e7d19720a71a43d7
jieiiia <stealth@bigmir.net>
2025-05-09 10:32:53 +0300

    Homework 3.1 - Print text to console

--- src/com/gmail/ponomarenko/homeworks/homework03/ShopOnline.java
+++ src/com/gmail/ponomarenko/homeworks/homework03/ShopOnline.java
@@ -1,4 +1,7 @@
 package com.gmail.ponomarenko.homeworks.homework03;
 
 public class ShopOnline {
+    public static void main(String[] args) {
+        System.out.println("This is shop-online app");
+    }
 }
