db22b2bdf621772dc3236b9c26863e518adad825
mariia <mariia@mail.uni-paderborn.de>
2025-05-12 16:29:57 +0200

    Homework 6.1 - Add scanner

--- src/com/gmail/ponomarenko/homeworks/homework06/Homework06.java
+++ src/com/gmail/ponomarenko/homeworks/homework06/Homework06.java
@@ -1,7 +1,11 @@
 package com.gmail.ponomarenko.homeworks.homework06;
 
+import java.util.Scanner;
+
 public class Homework06 {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number of rows: ");
+        int numberOfRows = scanner.nextInt();
     }
 }
