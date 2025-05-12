4296c79667156faa6c834887e0c339f6e37ae0c4
mariia <mariia@mail.uni-paderborn.de>
2025-05-12 17:02:27 +0200

    Homework 6.1 - Implementing a task with a while loop

--- src/com/gmail/ponomarenko/homeworks/homework06/Homework06.java
+++ src/com/gmail/ponomarenko/homeworks/homework06/Homework06.java
@@ -7,8 +7,10 @@ public class Homework06 {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the number of rows: ");
         int numberOfRows = scanner.nextInt();
-        forMethod(numberOfRows);
+        // forMethod(numberOfRows);
+        whileMethod(numberOfRows);
     }
+
     public static void forMethod(int numberOfRows) {
         int sum = 0;
         if (numberOfRows > 0) {
@@ -22,5 +24,22 @@ public class Homework06 {
             System.out.printf("Sum of numbers is %d", sum);
         }
     }
+
+    public static void whileMethod(int numberOfRows) {
+        int step = 1;
+        int sum = 0;
+        if (numberOfRows > 0) {
+            while (step <= numberOfRows) {
+                sum += step;
+                System.out.printf("%d) Num is %d, sum is %d \n", step, step, sum);
+                step++;
+            }
+            System.out.println("------------------------");
+            System.out.printf("Sum of numbers is %d", sum);
+        } else {
+            System.out.printf("Sum of numbers is %d", sum);
+        }
+    }
 }
+
 
