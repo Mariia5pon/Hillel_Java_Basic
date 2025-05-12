731cedac6667e9153cfa8c77a0c83966f1a63646
mariia <mariia@mail.uni-paderborn.de>
2025-05-12 16:54:32 +0200

    Homework 6.1 - Implementing a task with a for loop

--- src/com/gmail/ponomarenko/homeworks/homework06/Homework06.java
+++ src/com/gmail/ponomarenko/homeworks/homework06/Homework06.java
@@ -7,5 +7,20 @@ public class Homework06 {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the number of rows: ");
         int numberOfRows = scanner.nextInt();
+        forMethod(numberOfRows);
+    }
+    public static void forMethod(int numberOfRows) {
+        int sum = 0;
+        if (numberOfRows > 0) {
+            for (int i = 1; i <= numberOfRows; i++) {
+                sum += i;
+                System.out.printf("%d) Num is %d, sum is %d \n", i, i, sum);
+            }
+            System.out.println("------------------------");
+            System.out.printf("Sum of numbers is %d", sum);
+        } else {
+            System.out.printf("Sum of numbers is %d", sum);
+        }
     }
 }
+
