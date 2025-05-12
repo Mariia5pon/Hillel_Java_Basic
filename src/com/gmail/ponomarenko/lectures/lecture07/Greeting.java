3489276adf113ff993abfd9a0fcd9ab0f6acee56
mariia <mariia@mail.uni-paderborn.de>
2025-05-12 18:13:01 +0200

    Lecture 07 - Move files

---
+++ src/com/gmail/ponomarenko/lectures/lecture07/Greeting.java
@@ -0,0 +1,22 @@
+package com.gmail.ponomarenko.lectures.lecture07;
+
+public class Greeting {
+    public static void main(String[] args) {
+        //System.out.println("Hello, " + args[0]); ---- Array's length = 0
+        String name;
+        if (args.length > 0) {
+            name = args[0];
+        } else {
+            name = "anonymous";
+        }
+        System.out.println("Hello, " + name);
+    }
+
+    public static void print(String[] args) {
+        System.out.println("Income arguments: ");
+        for (String element : args) {
+            System.out.print(element + "\t");
+        }
+        System.out.println();
+    }
+}
