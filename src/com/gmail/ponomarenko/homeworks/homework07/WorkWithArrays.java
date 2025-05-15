3141230f13945e08183a6c797caf1a440fcc0634
mariia <mariia@mail.uni-paderborn.de>
2025-05-15 18:05:58 +0200

    Homework 7.1 - Add random-class

--- src/com/gmail/ponomarenko/homeworks/homework07/WorkWithArrays.java
+++ src/com/gmail/ponomarenko/homeworks/homework07/WorkWithArrays.java
@@ -1,13 +1,15 @@
 package com.gmail.ponomarenko.homeworks.homework07;
 
 import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class WorkWithArrays {
     public static void main(String[] args) {
-        scanner();
+        int numberOfIntegers = getNumberOfIntegers();
+        random(numberOfIntegers);
     }
 
-    public static void scanner() {
+    private static int getNumberOfIntegers() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the number of integers in the array: ");
         int numberOfIntegers = 0;
@@ -17,5 +19,18 @@ public class WorkWithArrays {
                 System.out.println("Please enter a number greater than zero.");
             }
         } while (numberOfIntegers <= 0);
+        return numberOfIntegers;
+    }
+
+    public static void random(int count) {
+        int[] array = new int[count];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = ThreadLocalRandom.current().nextInt(-100, 100);
+            System.out.println(array[i]);
+        }
+    }
+
+    public static void sumOfNegativeNumbers() {
+
     }
 }
