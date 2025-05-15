7cb39664bb92d4d3298b7e5209ecc6ef1f7eda2c
mariia <mariia@mail.uni-paderborn.de>
2025-05-15 17:46:42 +0200

    Homework 7.1 - Add scanner

--- src/com/gmail/ponomarenko/homeworks/homework07/WorkWithArrays.java
+++ src/com/gmail/ponomarenko/homeworks/homework07/WorkWithArrays.java
@@ -1,7 +1,21 @@
 package com.gmail.ponomarenko.homeworks.homework07;
 
+import java.util.Scanner;
+
 public class WorkWithArrays {
     public static void main(String[] args) {
+        scanner();
+    }
 
+    public static void scanner() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number of integers in the array: ");
+        int numberOfIntegers = 0;
+        do {
+            numberOfIntegers = scanner.nextInt();
+            if (numberOfIntegers <= 0) {
+                System.out.println("Please enter a number greater than zero.");
+            }
+        } while (numberOfIntegers <= 0);
     }
 }
