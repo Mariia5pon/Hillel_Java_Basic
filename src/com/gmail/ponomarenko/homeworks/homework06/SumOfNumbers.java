72c788cc64ce813e1129e51dfd705b1e639fff3b
mariia <mariia@mail.uni-paderborn.de>
2025-05-16 14:29:25 +0200

    Homework 6.1 - Create classes readInt and printResult

--- src/com/gmail/ponomarenko/homeworks/homework06/SumOfNumbers.java
+++ src/com/gmail/ponomarenko/homeworks/homework06/SumOfNumbers.java
@@ -4,26 +4,32 @@ import java.util.Scanner;
 
 public class SumOfNumbers {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the number of rows: ");
-        int numberOfRows = scanner.nextInt();
-        forMethod(numberOfRows);
-        whileMethod(numberOfRows);
-        doWhileMethod(numberOfRows);
+        int numberOfRows = readInt();
+        int sumFromFor = forMethod(numberOfRows);
+        printResult(sumFromFor);
+        int sumFromWhile = whileMethod(numberOfRows);
+        printResult(sumFromWhile);
+        int sumFromDoWhile = doWhileMethod(numberOfRows);
+        printResult(sumFromDoWhile);
     }
 
-    public static void forMethod(int numberOfRows) {
+    public static int readInt() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number of rows: ");
+        return scanner.nextInt();
+    }
+
+    public static int forMethod(int numberOfRows) {
         System.out.println("--= FOR LOOP =--");
         int sum = 0;
         for (int i = 1; i <= numberOfRows; i++) {
             sum += i;
             System.out.printf("%d) Num is %d, sum is %d \n", i, i, sum);
         }
-        System.out.println("------------------------");
-        System.out.printf("Sum of numbers is %d \n", sum);
+        return sum;
     }
 
-    public static void whileMethod(int numberOfRows) {
+    public static int whileMethod(int numberOfRows) {
         System.out.println("--= WHILE LOOP =--");
         int step = 1;
         int sum = 0;
@@ -32,11 +38,10 @@ public class SumOfNumbers {
             System.out.printf("%d) Num is %d, sum is %d \n", step, step, sum);
             step++;
         }
-        System.out.println("------------------------");
-        System.out.printf("Sum of numbers is %d \n", sum);
+        return sum;
     }
 
-    public static void doWhileMethod(int numberOfRows) {
+    public static int doWhileMethod(int numberOfRows) {
         System.out.println("--= DO-WHILE LOOP =--");
         int step = 1;
         int sum = 0;
@@ -47,8 +52,12 @@ public class SumOfNumbers {
                 step++;
             } while (step <= numberOfRows);
         }
+        return sum;
+    }
+
+    public static void printResult(int result) {
         System.out.println("------------------------");
-        System.out.printf("Sum of numbers is %d", sum);
+        System.out.printf("Sum of numbers is %d \n", result);
     }
 }
 
