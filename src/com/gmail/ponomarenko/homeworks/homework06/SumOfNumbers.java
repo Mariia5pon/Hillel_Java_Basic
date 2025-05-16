0ab53ca711589a360afb1f23bbd84d3fecab503b
mariia <mariia@mail.uni-paderborn.de>
2025-05-16 14:14:36 +0200

    Homework 6.1 - Rename the main class

--- src/com/gmail/ponomarenko/homeworks/homework06/SumOfNumbers.java
+++ src/com/gmail/ponomarenko/homeworks/homework06/SumOfNumbers.java
@@ -2,7 +2,7 @@ package com.gmail.ponomarenko.homeworks.homework06;
 
 import java.util.Scanner;
 
-public class Homework06 {
+public class SumOfNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the number of rows: ");
