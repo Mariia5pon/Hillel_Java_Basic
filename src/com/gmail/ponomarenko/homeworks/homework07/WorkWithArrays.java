14dcfeb9fb78696553d2047d29faf4c0a75631ec
mariia <mariia@mail.uni-paderborn.de>
2025-05-19 22:49:40 +0200

    Homework 7.1 - Remove redundant variables and classes

--- src/com/gmail/ponomarenko/homeworks/homework07/WorkWithArrays.java
+++ src/com/gmail/ponomarenko/homeworks/homework07/WorkWithArrays.java
@@ -10,29 +10,18 @@ public class WorkWithArrays {
         int[] array = createRandomArray(size);
         System.out.println("Elements of the array: " + Arrays.toString(array));
 
-        int negativeSum = getNegativeSum(array);
+        int negativeSum = negativeSum(array);
         System.out.println("Sum of negative numbers: " + negativeSum);
 
-        int evenCount = getEvenCount(array);
-        int oddCount = size - evenCount;
-        System.out.println("Count of even numbers: " + evenCount);
-        System.out.println("Count of odd numbers: " + oddCount);
-
-        int maxIndex = getMaxIndex(array);
-        int max = array[maxIndex];
-        System.out.printf("Largest element: %d (with index %d) \n", max, maxIndex);
-
-        int minIndex = getMinIndex(array);
-        int min = array[minIndex];
-        System.out.printf("Smallest element: %d (with index %d) \n", min, minIndex);
-
+        evenOddCounts(array);
+        maxMin(array);
         averageAfterFirstNegative(array);
     }
 
     private static int getSize() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the number of integers in the array: ");
-        int size = 0;
+        int size;
         do {
             size = scanner.nextInt();
             if (size <= 0) {
@@ -42,70 +31,61 @@ public class WorkWithArrays {
         return size;
     }
 
-    public static int[] createRandomArray(int count) {
-        int[] array = new int[count];
+    public static int[] createRandomArray(int size) {
+        int[] array = new int[size];
+
         for (int i = 0; i < array.length; i++) {
-            array[i] = ThreadLocalRandom.current().nextInt(-100, 100);
+            array[i] = ThreadLocalRandom.current().nextInt(-100, 101);
         }
         return array;
     }
 
-    public static int getNegativeSum(int[] array) {
+    public static int negativeSum(int[] array) {
         int sum = 0;
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] < 0) {
-                sum += array[i];
+        for (int j : array) {
+            if (j < 0) {
+                sum += j;
             }
         }
         return sum;
     }
 
-    public static int getEvenCount(int[] array) {
+    public static void evenOddCounts(int[] array) {
         int evenCount = 0;
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] % 2 == 0) {
+        for (int element : array) {
+            if (element % 2 == 0) {
                 evenCount++;
             }
         }
-        return evenCount;
+        System.out.println("Count of even numbers: " + evenCount);
+        System.out.println("Count of odd numbers: " + (array.length - evenCount));
     }
 
-    public static int getMaxIndex(int[] array) {
+    public static void maxMin(int[] array) {
         int maxIndex = 0;
+        int minIndex = 0;
         for (int i = 0; i < array.length; i++) {
             if (array[i] > array[maxIndex]) {
                 maxIndex = i;
-            }
-        }
-        return maxIndex;
-    }
-
-    public static int getMinIndex(int[] array) {
-        int minIndex = 0;
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] < array[minIndex]) {
+            } else if (array[i] < array[minIndex]) {
                 minIndex = i;
             }
         }
-        return minIndex;
+        System.out.printf("Largest element: %d (with index %d) \n", array[maxIndex], maxIndex);
+        System.out.printf("Smallest element: %d (with index %d) \n", array[minIndex], minIndex);
     }
 
     public static void averageAfterFirstNegative(int[] array) {
         int index = 0;
-        while (index < array.length) {
-            if (array[index] < 0) {
-                break;
-            }
+        while (index < array.length && array[index] >= 0) {
             index++;
         }
         if (index < array.length - 1) {
             double sum = 0;
-            int count = 0;
             for (int i = index + 1; i < array.length; i++) {
                 sum += array[i];
-                count++;
             }
-            double average = sum / count;
+            double average = sum / (array.length - index - 1);
             System.out.println("The average of the numbers after the first negative number: " + average);
         } else if (index == array.length - 1) {
             System.out.println("No numbers after the first negative.");
