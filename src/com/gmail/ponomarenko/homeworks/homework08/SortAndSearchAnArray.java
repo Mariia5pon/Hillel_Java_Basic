647c61a748ff1297771a004c4f69290b413c32ac
mariia <mariia@mail.uni-paderborn.de>
2025-05-22 16:55:31 +0200

    Homework 8.1 - Implementation of Binary Search

--- src/com/gmail/ponomarenko/homeworks/homework08/SortAndSearchAnArray.java
+++ src/com/gmail/ponomarenko/homeworks/homework08/SortAndSearchAnArray.java
@@ -5,6 +5,7 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class SortAndSearchAnArray {
     public static void main(String[] args) {
+        System.out.println("Enter the number of integers in the array: ");
         int size = readInt();
         int[] array = createRandomArray(size);
         System.out.printf("Elements of the array: ");
@@ -12,11 +13,18 @@ public class SortAndSearchAnArray {
         int[] sorted = insertionSort(array);
         System.out.printf("Sorted array: ");
         print(sorted);
+        System.out.println("Enter a number to search in the array: ");
+        int wanted = readInt();
+        int result = binarySearch(sorted, wanted);
+        if (result >= 0) {
+            System.out.printf("Number %d found in the sorted array at index %d.", wanted, result);
+        } else {
+            System.out.println("Target not found in array.");
+        }
     }
 
     private static int readInt() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the number of integers in the array: ");
         int size = 0;
         do {
             size = scanner.nextInt();
@@ -58,5 +66,22 @@ public class SortAndSearchAnArray {
             array[j] = temp;
         }
         return array;
+    }
+
+    public static int binarySearch(int[] array, int wanted) {
+        int left = 0;
+        int right = array.length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (array[mid] == wanted) {
+                return mid;
+            } else if (array[mid] < wanted) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
     }
 }
