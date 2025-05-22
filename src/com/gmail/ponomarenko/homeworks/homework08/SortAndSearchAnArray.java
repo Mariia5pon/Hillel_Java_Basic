c6c573671f9eed973343b491fccf880c88a336d2
mariia <mariia@mail.uni-paderborn.de>
2025-05-22 16:05:39 +0200

    Homework 8.1 - Rename the variables and arrays

--- src/com/gmail/ponomarenko/homeworks/homework08/SortAndSearchAnArray.java
+++ src/com/gmail/ponomarenko/homeworks/homework08/SortAndSearchAnArray.java
@@ -6,29 +6,42 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class SortAndSearchAnArray {
     public static void main(String[] args) {
-        int numberOfIntegers = getNumberOfIntegers();
-        int[] arrayOfIntegers = createRandomArray(numberOfIntegers);
-        System.out.println("Elements of the array: " + Arrays.toString(arrayOfIntegers));
+        int size = readInt();
+        int[] array = createRandomArray(size);
+        System.out.printf("Elements of the array: ");
+        print(array);
     }
 
-    private static int getNumberOfIntegers() {
+    private static int readInt() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the number of integers in the array: ");
-        int numberOfIntegers = 0;
+        int size = 0;
         do {
-            numberOfIntegers = scanner.nextInt();
-            if (numberOfIntegers <= 0) {
+            size = scanner.nextInt();
+            if (size <= 0) {
                 System.out.println("Please enter a number greater than zero.");
             }
-        } while (numberOfIntegers <= 0);
-        return numberOfIntegers;
+        } while (size <= 0);
+        return size;
     }
 
     public static int[] createRandomArray(int count) {
         int[] array = new int[count];
         for (int i = 0; i < array.length; i++) {
-            array[i] = ThreadLocalRandom.current().nextInt(-100, 100);
+            array[i] = ThreadLocalRandom.current().nextInt(1, 101);
         }
         return array;
+    }
+
+    private static void print(int[] array) {
+        System.out.print("[");
+        if (array.length > 0) {
+            System.out.print(array[0]);
+        }
+        for (int i = 1; i < array.length; i++) {
+            int element = array[i];
+            System.out.print(", " + element);
+        }
+        System.out.println("]");
     }
 }
