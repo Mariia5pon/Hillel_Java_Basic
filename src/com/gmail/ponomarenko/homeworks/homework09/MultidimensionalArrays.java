53500870664e7826caf7555b2f4cdace4b935e51
mariia <mariia@mail.uni-paderborn.de>
2025-05-22 17:14:38 +0200

    Homework 9.1 - Ask for the matrix size

--- src/com/gmail/ponomarenko/homeworks/homework09/MultidimensionalArrays.java
+++ src/com/gmail/ponomarenko/homeworks/homework09/MultidimensionalArrays.java
@@ -1,7 +1,23 @@
 package com.gmail.ponomarenko.homeworks.homework09;
 
+import java.util.Scanner;
+
 public class MultidimensionalArrays {
     public static void main(String[] args) {
+        int rows = readInt("rows");
+        int columns = readInt("columns");
+    }
 
+    public static int readInt(String string) {
+        System.out.printf("Enter the number of %s: ", string);
+        Scanner scanner = new Scanner(System.in);
+        int size = 0;
+        do {
+            size = scanner.nextInt();
+            if (size <= 0) {
+                System.out.println("Please enter a number greater than zero.");
+            }
+        } while (size <= 0);
+        return size;
     }
 }
