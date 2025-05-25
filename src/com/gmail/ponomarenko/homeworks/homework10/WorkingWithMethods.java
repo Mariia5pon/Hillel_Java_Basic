0a1e63ba838a8bd7a053387638a8dc15160c7d8b
mariia <mariia@mail.uni-paderborn.de>
2025-05-25 18:52:55 +0200

    Homework 10.1 - Print the volume of the cylinder.

--- src/com/gmail/ponomarenko/homeworks/homework10/WorkingWithMethods.java
+++ src/com/gmail/ponomarenko/homeworks/homework10/WorkingWithMethods.java
@@ -5,7 +5,10 @@ import java.util.Scanner;
 public class WorkingWithMethods {
     public static void main(String[] args) {
         int number = readInt("1. Enter an integer number: ");
-        System.out.printf("\tThe square of %d is %d.", number, square(number));
+        System.out.printf("\tThe square of %d is %d.\n", number, square(number));
+        double radius = readInt("2. Enter the radius of the cylinder: ");
+        double height = readInt("\tEnter the height of the cylinder: ");
+        System.out.printf("\tThe volume of a cylinder with a radius of %f and a height of %f is %f.", radius, height, cylinderVolume(radius, height));
     }
 
     public static int readInt(String string) {
@@ -17,5 +20,9 @@ public class WorkingWithMethods {
 
     public static int square(int arg) {
         return arg * arg;
+    }
+
+    public static double cylinderVolume(double radius, double height) {
+        return 3.14 * radius * radius * height;
     }
 }
