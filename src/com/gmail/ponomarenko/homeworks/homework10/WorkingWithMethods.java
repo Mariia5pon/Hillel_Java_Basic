a00c50e41ab2b45ee8e0209ab9e4524fea03ffbc
mariia <mariia@mail.uni-paderborn.de>
2025-05-25 18:14:56 +0200

    Homework 10.1 - Ask a number.

--- src/com/gmail/ponomarenko/homeworks/homework10/WorkingWithMethods.java
+++ src/com/gmail/ponomarenko/homeworks/homework10/WorkingWithMethods.java
@@ -3,5 +3,14 @@ package com.gmail.ponomarenko.homeworks.homework10;
 import java.util.Scanner;
 
 public class WorkingWithMethods {
+    public static void main(String[] args) {
+        int number = readInt();
+    }
 
+    public static int readInt() {
+        System.out.println("Enter an integer number.");
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        return number;
+    }
 }
