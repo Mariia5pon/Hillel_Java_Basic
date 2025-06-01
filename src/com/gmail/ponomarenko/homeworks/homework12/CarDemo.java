ff0a3068769a9d96608dd61521c21c6ee12ed1af
mariia <mariia@mail.uni-paderborn.de>
2025-06-01 15:47:28 +0200

    Homework 12.1 - Create CarDemo object and use start() method.

---
+++ src/com/gmail/ponomarenko/homeworks/homework12/CarDemo.java
@@ -0,0 +1,8 @@
+package com.gmail.ponomarenko.homeworks.homework12;
+
+public class CarDemo {
+    public static void main(String[] args) {
+        Car myCar = new Car();
+        myCar.start();
+    }
+}
