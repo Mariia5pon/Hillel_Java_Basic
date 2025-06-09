6d7791bbe7d2a6c3769c8e9588d6c3e7657aedbe
Mariia Ponomarenko <mariia5pon@gmail.com>
2025-06-09 16:35:28 +0200

    Homework 13.1 - Implement the interface: square.

---
+++ src/com/gmail/ponomarenko/homeworks/homework13/Square.java
@@ -0,0 +1,14 @@
+package com.gmail.ponomarenko.homeworks.homework13;
+
+public class Square implements Shape {
+    private double side;
+
+    public Square(double side) {
+        this.side = side;
+    }
+
+    @Override
+    public double area() {
+        return side * side;
+    }
+}
